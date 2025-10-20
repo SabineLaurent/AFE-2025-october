c975352026cd8c0319344f41714d45cdff4b133e
SabineLaurent <sabinelaurent.dev@gmail.com>
2025-10-20 13:50:50 +0200

    Initial Commit

---
+++ Workout_time/Workout_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_timeApp.swift
+//  Workout_time
+//
+//  Created by Sabine Laurent on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Workout_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
